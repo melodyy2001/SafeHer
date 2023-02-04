08278c4f88f8dabc990415c55f0387c0d2129fb6
melodyyin <melody.yin2001@utexas.edu>
2023-02-04 10:53:53 -0600

    First Commit

---
+++ app/src/main/java/com/wixHack/safeher/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wixHack.safeher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

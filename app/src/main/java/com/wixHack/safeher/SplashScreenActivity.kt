e7b226b7da075ad1ac6b3ace34c05720c96f8041
melodyyin <melody.yin2001@utexas.edu>
2023-02-04 16:57:35 -0600

    Finished splash screen

--- app/src/main/java/com/wixHack/safeher/SplashScreenActivity.kt
+++ app/src/main/java/com/wixHack/safeher/SplashScreenActivity.kt
@@ -1,11 +1,22 @@
 package com.wixHack.safeher
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val logo = findViewById<ImageView>(R.id.logo)
+        logo.alpha = 0f
+        logo.animate().setDuration(1500).alpha(1f).withEndAction{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
     }
 }
